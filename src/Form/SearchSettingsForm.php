97c27b14a5ff61250086afe12e77c1f2481882f6
Bryn Jones <bryn.jones@reckless.agency>
2017-01-24 11:11:40 +0000

    Add exclude content type bundles from Drupal core search functionality.
    Signed-off-by: Bryn Jones <bryn.jones@reckless.agency>

---
+++ src/Form/SearchSettingsForm.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\starter\Form\SearchSettingsForm.
+ */
+
+namespace Drupal\starter\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class SearchSettings.
+ *
+ * @package Drupal\starter\Form
+ */
+class SearchSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'exclude_content_types_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'exclude_content_types.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('exclude_content_types.settings');
+    $configured_bundles = $config->get('bundles') ? $config->get('bundles') : [];
+
+    // get list of current Content Types
+    $node_bundles = \Drupal::service('entity.manager')->getStorage('node_type')->loadMultiple();
+
+    $options = [];
+    foreach ($node_bundles as $bundle) {
+      $options[$bundle->id()] = $bundle->label();
+    }
+
+    $form['bundles'] = array(
+      '#type' => 'checkboxes',
+      '#options' => $options,
+      '#title' => t('Content types to exclude from Drupal core search'),
+      '#default_value' => $configured_bundles,
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('exclude_content_types.settings')
+      ->set('bundles', $form_state->getValue('bundles'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
