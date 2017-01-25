01126e08017c6fcf38250453fe13eddc0cfa52e0
Bryn Jones <bryn.jones@reckless.agency>
2017-01-25 15:23:26 +0000

    Add option to pre-populate entity form fields, via request parameters.
    Signed-off-by: Bryn Jones <bryn.jones@reckless.agency>

--- src/Form/StarterSettingsForm.php
+++ src/Form/StarterSettingsForm.php
@@ -71,6 +71,17 @@ class StarterSettingsForm extends ConfigFormBase {
       '#description' => $this->t('Entities that contain this string as part of their path alias will only be directly accessible to logged in users.'),
     );
 
+    $form['form'] = array(
+      '#type' => 'fieldset',
+      '#title' => $this->t('Form settings'),
+    );
+
+    $form['form']['prepopulate'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Allow entity form fields to be pre-populated via request parameters?'),
+      '#default_value' => $starter_config->get('prepopulate'),
+    );
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -90,6 +101,7 @@ class StarterSettingsForm extends ConfigFormBase {
       ->set('paths.login', $form_state->getValue('login_path'))
       ->set('paths.logout', $form_state->getValue('logout_path'))
       ->set('paths.disable_access', str_replace('/','-',$form_state->getValue('disable_access_path')))
+      ->set('prepopulate', $form_state->getValue('prepopulate'))
       ->save();
 
     parent::submitForm($form, $form_state);
